94181a3c0633ce0b983e8f852a3450269b51d047
Rupert Ong <rupert.ong@gmail.com>
2017-10-19 20:34:57 -0400

    Update index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var notes = require('../models/notes-memory')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  notes.keylist()
+    .then(keylist => {
+      var keyPromises = [];
+      for (var key of keylist) {
+        keyPromises.push(
+          notes.read(key)
+            .then(note => {
+              return { key: note.key, title: note.title }
+            })
+        );
+      }
+      return Promise.all(keyPromises);
+    })
+    .then(notelist => {
+      res.render('index', { title: 'Notes', notelist: notelist });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
