5bd264cd9e16a9db36b977bd24d0b8262ba81793
Rupert Ong <rupert.ong@gmail.com>
2017-10-20 21:36:47 -0400

    Add save note route

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var util = require('util');
-var express = require('express');
-var router = express.Router();
-var notes = require('../models/notes-memory');
+const util = require('util');
+const express = require('express');
+const router = express.Router();
+const notes = require('../models/notes-memory');
 
 router.get('/add', (req, res, next) => {
   res.render('noteedit', {
@@ -11,6 +11,21 @@ router.get('/add', (req, res, next) => {
     docreate: true,
     notekey: '',
     note: undefined
+  });
+});
+
+router.post('/save', (req, res, next) => {
+  let p;
+  if (req.body.docreate === 'create') {
+    p = notes.create(req.body.notekey, req.body.title, req.body.body);
+  } else {
+    p = notes.update(req.body.notekey, req.body.title, req.body.body);
+  }
+  
+  p.then(note => {
+    res.redirect('/notes/view?key=' + req.body.notekey);
+  }).catch(err => {
+    next(err);
   });
 });
 
