035eabea007231585fd02e08c7a8f66819fce6be
Rupert Ong <rupert.ong@gmail.com>
2017-11-12 10:30:51 -0500

    Add log file functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
@@ -11,13 +12,28 @@ const notes = require('./routes/notes');
 
 const app = express();
 
+// Archive log file periodically if env var specified
+const FileStreamRotator = require('file-stream-rotator');
+let accessLogStream;
+if (process.env.REQUEST_LOG_FILE) {
+  let logDirectory = path.dirname(process.env.REQUEST_LOG_FILE);
+  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+  accessLogStream = FileStreamRotator.getStream({
+    filename: process.env.REQUEST_LOG_FILE,
+    frequency: 'daily',
+    verbose: false
+  })
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
+  stream: accessLogStream ? accessLogStream : process.stdout
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -30,14 +46,14 @@ app.use('/', routes);
 app.use('/notes', notes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
