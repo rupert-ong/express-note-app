34d4a86f77e50c6b2f1d045c8044b97fa282b9dd
Rupert Ong <rupert.ong@gmail.com>
2017-11-12 21:10:47 -0500

    Add JSON methods to Note data object

--- models/Note.js
+++ models/Note.js
@@ -6,4 +6,25 @@ module.exports = class Note {
     this.title = title;
     this.body = body;
   }
+
+  /**
+   * Returns stringified JSON representation of Note instance
+   */
+  get JSON() {
+    return JSON.stringify({
+      key: this.key,
+      title: this.title,
+      body: this.body
+    })
+  }
+
+  /**
+   * Creates Note instance from JSON string
+   * @param {String} json JSON string to be parsed into data object to create Note instance
+   */
+  static fromJSON(json) {
+    let data = JSON.parse(json);
+    let note = new Note(data.key, data.title, data.body);
+    return note;
+  }
 }
